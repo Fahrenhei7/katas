805956799b696ae6b51f6e216b0396c039de39dc
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-10-14 21:59:27 +0300

    PascalCase to snake_case

---
+++ ruby/pascal_case_to_snake_case.rb
@@ -0,0 +1,20 @@
+# Complete the function/method so that it takes CamelCase string and returns
+# the string in snake_case notation. Lowercase characters can be numbers.
+# If method gets number, it should return string.
+
+# returns test_controller
+# to_underscore('TestController')
+
+# returns movies_and_books
+# to_underscore('MoviesAndBooks')
+
+# returns app7_test
+# to_underscore('App7Test')
+
+# returns "1"
+# to_underscore(1)
+
+
+def to_underscore(string)
+  string.to_s.split(/(?=[A-Z])/).join('_').downcase
+end
